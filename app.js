65df8b2113fd2ad21cd8930877d5583e3b485efa
João Bisneto <joao.bisneto@mapesolutions.com>
2022-03-17 01:03:28 -0300

    Finished code.

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const fetchPokemon = () => {
   const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
+  const pokemonPromises = []
+
   for(let i = 1; i <= 150; i++){
-    fetch(getPokemonUrl(i)).then(response => response.json().then(pokemon => console.log(pokemon)))
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
   }
+
+  Promise.all(pokemonPromises)
+  .then(pokemons => {
+
+    const lisPokemons = pokemons.reduce((acc, pokemon) => {
+      const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+      acc += `
+        <li class="card ${types[0]}">
+          <img class="card-image" 
+            alt="${pokemon.name}"
+            src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg" 
+          />
+          <h2 class="card-title"> ${pokemon.id}. ${pokemon.name} </h2>
+          <p class="card-subtitle"> ${types.join(' | ')} </p>
+        </li>
+      `
+      return acc
+    }, '')
+
+    const ul = document.querySelector('[data-js="pokedex"]')
+
+    ul.innerHTML = lisPokemons
+  })
 
 }
 
