4871a9395f01f2ebfb1e1b518e579ee82a61f532
João Bisneto <joao.bisneto@mapesolutions.com>
2022-03-17 01:12:50 -0300

    Refatorando a função de geração para utilizar o retorno implícito do reduce.

--- app.js
+++ app.js
@@ -3,24 +3,22 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 const generatePokemonPromisses = () => Array(150).fill().map((_, index) => 
   fetch(getPokemonUrl(index+1)).then(response => response.json()))
 
-const generateHTML = pokemons => {
-  return pokemons.reduce((acc, pokemon) => {
-    const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+const generateHTML = pokemons => pokemons.reduce((acc, pokemon) => {
+  const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
-    acc += `
-      <li class="card ${types[0]}">
-        <img class="card-image" 
-          alt="${pokemon.name}"
-          src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg" 
-        />
-        <h2 class="card-title"> ${pokemon.id}. ${pokemon.name} </h2>
-        <p class="card-subtitle"> ${types.join(' | ')} </p>
-      </li>
-    `
-    return acc
-  }, '')
+  acc += `
+    <li class="card ${types[0]}">
+      <img class="card-image" 
+        alt="${pokemon.name}"
+        src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg" 
+      />
+      <h2 class="card-title"> ${pokemon.id}. ${pokemon.name} </h2>
+      <p class="card-subtitle"> ${types.join(' | ')} </p>
+    </li>
+  `
+  return acc
+}, '')
 
-}
 
 const insertPokemonsIntoPage = pokemons => {
   const ul = document.querySelector('[data-js="pokedex"]')
